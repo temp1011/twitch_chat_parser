72e18d61687cc34ad4436768211f700984bbe6f3
temp1011 <temp1011github@gmail.com>
2019-04-12 14:55:27 +0100

    reduce unwrap usage and move to more stream based method

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use irc::client::prelude::*;
 use irc::proto::message::Tag;
 
 fn main() {
-    // We can also load the Config at runtime via Config::load("path/to/config.toml")
     let config = Config {
         nickname: Some("justinfan123123".to_owned()),
         server: Some("irc.chat.twitch.tv".to_owned()),
@@ -14,16 +13,20 @@ fn main() {
 
     let mut reactor = IrcReactor::new().unwrap();
     let client = reactor.prepare_client_and_connect(&config).unwrap();
-     client.send_cap_req(&[irc::proto::caps::Capability::Custom("twitch.tv/tags")]).unwrap(); 
+    client.send_cap_req(&[irc::proto::caps::Capability::Custom("twitch.tv/tags")]).unwrap();
     client.identify().unwrap();
 
     reactor.register_client_with_handler(client, |client, message| {
         match message.command {
             Command::PRIVMSG(ref target, ref msg) => {
-                print!("{}, {}, {:?}\n\n\n", msg, target, format_tag(message.tags.unwrap()));
+                let tags = match message.tags {
+                    Some(t) => format_tags(t),
+                    _ => Vec::with_capacity(0)
+                };
+                println!("{}, {}, {:?}", msg, target, tags);
             },
-            Command::PING(ref target, ref msg) => {
-                client.send_pong(msg.clone().unwrap_or(String::from(""))).unwrap();
+            Command::PING(target, msg) => {
+                client.send_pong(msg.unwrap_or(String::from("")))?;
             },
             Command::JOIN(ref chan, _, _) => println!("joined {}", chan),
             _ => {} //dbg!(message.command)
@@ -31,18 +34,18 @@ fn main() {
         Ok(())
     });
 
-    reactor.run().unwrap();
+    reactor.run();
 }
 
-//TODO - stream this instead? (map etc)
-fn format_tag(tags: Vec<Tag>) -> Vec<String> {
-    let mut ret: Vec<String> = Vec::with_capacity(tags.len());
-    for t in tags {
+//probably should be json or something fancy
+fn format_tags(tags: Vec<Tag>) -> Vec<String> {
+    tags.into_iter()
+        .map(|t| {
         let mut s1 = t.0;
-        let s2 = &t.1.unwrap();
+        let s2 = t.1.unwrap_or(String::from(""));
         s1.push_str("=");
-        s1.push_str(s2);
-        ret.push(s1);
-    }
-    ret
+        s1.push_str(&s2);
+        s1
+    })
+    .collect()
 }
