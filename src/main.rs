d78d09b6015c8543071b3518cb02655273c58140
temp1011 <temp1011github@gmail.com>
2019-04-13 12:12:59 +0100

    use options in struct and don't serialize if they are none

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,55 @@
 extern crate irc;
 extern crate serde_derive;
 extern crate serde_json;
+extern crate serde_with_macros;
 
 use irc::client::prelude::*;
 use irc::error::IrcError;
 use irc::proto::message::Tag;
 
 use serde_derive::{Deserialize, Serialize};
+use serde_with_macros::skip_serializing_none;
 
 use std::error::Error;
 
 //https://dev.twitch.tv/docs/irc/tags/#privmsg-twitch-tags
 //deprecated tags not serialised
-//TODO - optional fields?
+#[skip_serializing_none]
 #[derive(Debug, Serialize, Deserialize, Default)]
 struct TwitchTags {
-    badge_info: String,
-    badges: Vec<String>,
-    bits: u64,      //0 if not bits message
-    colour: String, //hex rgb
-    display_name: String,
-    emotes: Vec<String>,
-    id: String,
-    moderator: bool,
-    room_id: u64,
-    tmi_sent_ts: u64, //safe for 2038 problem
-    user_id: String,
+    badge_info: Option<String>,
+    badges: Option<Vec<String>>,
+    bits: Option<u64>,
+    colour: Option<String>, //hex rgb
+    display_name: Option<String>,
+    emotes: Option<Vec<String>>,
+    id: Option<String>,
+    moderator: Option<bool>,
+    room_id: Option<u64>,
+    tmi_sent_ts: Option<u64>, //safe for 2038 problem
+    user_id: Option<String>,
 }
 
-fn s_or_empty(s: Option<String>) -> String {
-    if let Some(v) = s {
-        return v;
-    }
-    String::from("")
+fn map_to_int(s: String) -> u64 {
+    s.parse::<u64>().unwrap_or(0)
 }
 
-fn i_or_zero(s: Option<String>) -> u64 {
-    if let Some(i) = s {
-        return i.parse::<u64>().unwrap_or(0);
-    }
-    0
-}
-
-fn vec_or_empty(s: Option<String>, split: char) -> Vec<String> {
-    if let Some(v) = s {
-        return v.split(split).map(String::from).collect();
-    }
-    Vec::with_capacity(0)
-}
-
-//TODO - don't bother setting field again if option not present, not sure best way to do this
-//if struct stores options can do this with option::map
 fn get_tags_struct(tags: Vec<Tag>) -> TwitchTags {
     let mut ret: TwitchTags = Default::default();
     for t in tags.into_iter() {
+        let val = t.1.filter(|x| !x.is_empty());
         match t.0.as_str() {
-            "badge-info" => ret.badge_info = s_or_empty(t.1),
-            "badges" => ret.badges = vec_or_empty(t.1, ','),
-            "bits" => ret.bits = i_or_zero(t.1),
-            "color" => ret.colour = s_or_empty(t.1),
-            "display-name" => ret.display_name = s_or_empty(t.1),
-            "emotes" => ret.emotes = vec_or_empty(t.1, '/'),
-            "id" => ret.id = s_or_empty(t.1),
-            "mod" => ret.moderator = i_or_zero(t.1) != 0,
-            "room-id" => ret.room_id = i_or_zero(t.1),
-            "tmi-sent-ts" => ret.tmi_sent_ts = i_or_zero(t.1),
-            "user-id" => ret.user_id = s_or_empty(t.1),
+            "badge-info" => ret.badge_info = val,
+            "badges" => ret.badges = val.map(|s| s.split(',').map(String::from).collect()),
+            "bits" => ret.bits = val.map(map_to_int),
+            "color" => ret.colour = val,
+            "display-name" => ret.display_name = val,
+            "emotes" => ret.emotes = val.map(|s| s.split('/').map(String::from).collect()),
+            "id" => ret.id = val,
+            "mod" => ret.moderator = val.map(map_to_int).map(|i| i != 0),
+            "room-id" => ret.room_id = val.map(map_to_int),
+            "tmi-sent-ts" => ret.tmi_sent_ts = val.map(map_to_int),
+            "user-id" => ret.user_id = val,
             _ => {}
         }
     }
